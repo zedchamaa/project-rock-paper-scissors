8784bc5c31fb28becccdb8d0a42f5777e0115f4c
Zed Chamaa <sales@nineprovince.com>
2022-06-08 14:22:53 +0200

    Create a function to play one round of the game

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 // Create a variable to store the player's selection when prompted
 
 let playerSelection = prompt("Enter Rock, Paper or Scissors", );
-console.log(playerSelection);
 
 // Create a function to store the computer's random selection
 
@@ -14,5 +13,17 @@ function computerPlay() {
     } else if (random === 2) {
       return "paper";
     } else return "scissors";
+}
+
+// Create a function to play one round of the game
+
+function playRound(playerSelection, computerPlay) {
+  if (playerSelection === "rock" && computerPlay === "scissors") {
+    return "You Lose! Rock beats Scissors.";
+  } else if (playerSelection === "scissors" && computerPlay === "paper") {
+    return "You Lose! Scissors beat Paper.";
+  } else if (playerSelection === "rock" && computerPlay === "paper") {
+    return "You Lose! Paper beats Rock.";
+  }
 }
 
