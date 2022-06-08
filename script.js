29216892a0e28b9e20cc33c709d946c2fe006840
Zed Chamaa <sales@nineprovince.com>
2022-06-08 13:58:29 +0200

    Create a function to store the computer's random selection

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-// Create a variable to store the player's selection when prompted.
+
+
+// Create a variable to store the player's selection when prompted
 
 let playerSelection = prompt("Enter Rock, Paper or Scissors", );
 console.log(playerSelection);
+
+// Create a function to store the computer's random selection
+
+function computerPlay() {
+  let random = Math.floor(Math.random() * 3) + 1;
+    if (random === 1) {
+      return "rock";
+    } else if (random === 2) {
+      return "paper";
+    } else return "scissors";
+}
+
