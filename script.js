da76b91ee97551c40b412e7bbc473a997dc7189b
Zed Chamaa <sales@nineprovince.com>
2022-06-15 20:00:45 +0200

    Create a better version of the rock, paper & scissors game

--- script.js
+++ script.js
@@ -1,83 +1,70 @@
-// The choices and round scores are stores in arrays
+// Create variables for player vs computer
 
-const choices = ["rock", "paper", "scissors"];
-const roundScores = [];
+let player;
+let playerScore = 0;
+let computer;
+let computerScore = 0;
 
-// The total score of one completed game is stored in an object
+// Create an array to store the choices
 
-const count = {};
+let choices = ["rock", "paper", "scissors"];
 
-// When called, this function will play one full game
+// Create a function to populate the 3 choices into the player-choices div 
 
-function game() {
-  for (let i = 0; i < 5; i++) {
-    playRound(i + 1);
+window.onload = function() {
+  for (let i = 0; i <3; i++) {
+    let choice = document.createElement("img");
+    choice.id = choices[i];
+    choice.src = choices[i] + ".png";
+    choice.addEventListener("click", selectChoice);
+    document.getElementById("player-choices").append(choice);
   }
-  console.log(roundScores);
-  console.log(totalScore());
 }
 
-// When called, this function plays one round between the player and the computer
+// Create a function to select the choices of the player and computer, and then update their score
 
-function playRound(round) {
-  const playerSelection = playerChoice();
-  const computerSelection = computerChoice();
-  const winner = checkWinner(playerSelection, computerSelection);
-  console.log("Round: " + round);
-  console.log("Player: " + playerSelection);
-  console.log("Computer: " + computerSelection);
-  console.log(winner);
-  if (winner === "Player Wins!") {
-    roundScores.push("player");
-  } else if (winner === "Computer Wins!") {
-    roundScores.push("computer");
-  } else roundScores.push("tie");
-  console.log("---------------------------------------------------------");
-}
+function selectChoice() {
+  player = this.id;
+  document.getElementById("player-choice").src = player + ".png";
 
-// When called, this function prompts the player to enter a choice between rock, paper or scissors 
+  // random choice for computer
+  computer = choices[Math.floor(Math.random() * 3)];
+  document.getElementById("computer-choice").src = computer + ".png";
 
-function playerChoice() {
-  let input = prompt("Enter rock, paper or scissors", );
-  input = input.toLowerCase();
-  if (
-    input === choices[0] ||
-    input === choices[1] ||
-    input === choices[2]) {
-      return input;
+  // check for winner
+  if (player === computer) {
+    playerScore += 1;
+    computerScore += 1;
+  }
+
+  else {
+    if (player === "rock") {
+      if (computer === "paper") {
+        computerScore += 1;
+      }
+      else if (computer === "scissors") {
+        playerScore += 1;
+      }
     }
-  else prompt("Enter rock, paper or scissors", );
+    else if (player === "paper") {
+      if (computer === "rock") {
+        playerScore += 1;
+      }
+      else if (computer === "scissors") {
+        computerScore += 1;
+      }
+    }
+    else if (player === "scissors") {
+      if (computer === "rock") {
+        computerScore += 1;
+      }
+      else if (computer === "paper") {
+        playerScore += 1;
+      }
+    }
+  }
+
+  // update score
+  document.getElementById("player-score").innerText = playerScore;
+  document.getElementById("computer-score").innerText = computerScore;
 }
-
-// When called, this function will randomly generate the computer's choice between rock, paper and scissors
-
-function computerChoice() {
-  return choices[Math.floor(Math.random() * choices.length)];
-}
-
-// When called, this function will check the winner between the player and the computer
-
-function checkWinner(playerChoice, computerChoice) {
-  if (playerChoice === computerChoice) {
-    return "It\'s a tie";
-    } else if (playerChoice === "rock" && computerChoice === "scissors") {
-        return "Player Wins!";
-    } else if (playerChoice === "paper" && computerChoice === "rock") {
-        return "Player Wins!"; 
-    } else if (playerChoice === "scissors" && computerChoice === "paper") {
-        return "Player Wins!";
-    } else return "Computer Wins!"
-}
-
-// When called, this function will store the final score
-
-function totalScore() {
-  roundScores.forEach(element => {
-    count[element] = (count[element] || 0) + 1;
-  });
-  console.log(count);
-}
-
-// This function call triggers the game to start
-
-game();
